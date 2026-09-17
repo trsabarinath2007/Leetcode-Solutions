1f3148a9739f8c1aee22c85e3c998aac5919862d
Sabarinath TR <trsabarinath2007@gmail.com>
2026-09-17 08:42:06 +0530

    Time: 5 ms (83.07%) | Memory: 45.9 MB (71.84%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int temp=x;
+        int rev=0;
+        while(x>0){
+            int digit=x%10;
+            rev=(rev*10)+digit;
+            x/=10;
+        }
+        return temp==rev;
+    }
+}
